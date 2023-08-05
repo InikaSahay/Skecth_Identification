054de8c69f824238060828162887b12dbfd7b303
InikaSahay <87757134+InikaSahay@users.noreply.github.com>
2023-08-05 16:56:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+function preload(){
+    classifier= ml5.imageClassifier("DoodleNet");
+}
+
+function setup(){
+    canvas=createCanvas(280,280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
+}
+
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+    if (mouseIsPressed){
+        line(pmouseX,pmouseY, mouseX, mouseY);
+    }
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult);
+}
+
+function gotResult(error,result){
+    if (error){
+        console.error(error);
+    }    
+    console.log(result);
+    document.getElementById("label").innerHTML="label: "+result[0].label;
+    document.getElementById("confidence").innerHTML="confidence: "+Math.round(result[0].confidence*100)+"%";
+    utterThis=new SpeechSynthesisUtterance(result[0].label);
+    synth.speak(utterThis);
+}
+
+function clearCanvas(){
+    background("white");
+}
